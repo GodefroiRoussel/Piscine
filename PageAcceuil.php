1e705d5bcba91a7fac81552ff0bdd01ae0e7cb49
Loriszirah <loris.zirah@etu.umontpellier.fr>
2016-11-28 00:16:58 +0100

    Add files via upload

---
+++ PageAcceuil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Test de Hollande</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<h1>Bienvenue sur le test de Hollande</h1>
+		<div>
+			<a href="ConnexionEtudiant.php" class="btn btn-info">Etudiant</a> 
+		</div>
+		<br />
+		<div>
+			<a href="ConnexionAdmin.php" class=btn btn-info">Admin</a>
+		</div>
+	</body>
+</html>
+
