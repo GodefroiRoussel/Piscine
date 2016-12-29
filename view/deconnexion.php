c465171a873789c05f9c23af45f1c49b65fd9749
Gunners69400 <escalier.justin@gmail.com>
2016-12-29 13:26:09 +0100

    Add: Ajout des pages Admin manquantes

---
+++ view/deconnexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Test de Hollande</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<section>
+    <p>Déconnexion</p>
+    <a href="PageAccueil.php" class="btn btn-info">Accueil</a>
+</section>
+</body>
+</html>
---
+++ view/modifierPromo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Test de Hollande</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<section>
+    <a href="PageAdmin.php" class="btn btn-info">Retour</a>
+</section>
+</body>
+</html>
