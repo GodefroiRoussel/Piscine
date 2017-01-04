f2f6e268a45f34fbd90b4a2a6b433445655d0b6d
GodefroiRoussel <godefroi.roussel@etu.umontpellier.fr>
2017-01-04 11:02:14 +0100

    Update: Vérification javascript pour l'ajout d'un admin qu'il ne manque pas de données

--- controller/js/ajoutAdmin.js
+++ controller/js/ajoutAdmin.js
@@ -3,11 +3,11 @@ function informationsCorrecte(){
 	But : Vérifie si les champs email et passwd ne sont pas vides
 	*/
 	res=true;
-	if(document.getElementById(email).value()==""){
+	if(document.getElementById('email').value==""){
 		alert("Email non renseigné");
 		res= false;
 	}
-	if(document.getElementById(passwd).value()==""){
+	if(document.getElementById('passwd').value==""){
 		alert("Mot de passe non renseigné");
 		res=false;
 	}
