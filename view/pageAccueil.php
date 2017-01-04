990fdfc2719e1fdaf822bbcaab3e6d5a02c0919e
GodefroiRoussel <godefroi.roussel@etu.umontpellier.fr>
2017-01-04 09:06:09 +0100

    Add: le lien pour faire charger bootstrap via un CDN

--- view/pageAccueil.php
+++ view/pageAccueil.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8"/>
 		<title>Test de Hollande</title>
 		<link rel="stylesheet" href="style.css" />
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
 	<body>
 		<header>
