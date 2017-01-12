dc54f89d5840f452e18c2606c8ee0aac4868ed6a
chloedalger <ch.dalger@gmail.com>
2017-01-12 17:06:51 +0100

    Add files via upload

---
+++ controller/departement.controller.php
@@ -0,0 +1,42 @@
+<?php
+  require_once('../vendor/autoload.php');
+  require_once('../model/token.php');
+  require_once('../model/connexionBD.php');
+  require_once('../model/Admin.php');
+  require_once('../model/Etudiant.php');
+  require_once('../model/Promo.php');
+  require_once('../model/fiche.php');
+  require_once('../model/Departement.php');
+  require_once('../assets/functions/calculResultat.php');
+  use \Firebase\JWT\JWT;
+  //TODO: mettre dans un fichier .env
+  $key = "ceSera1cLERiasEcP0UrP1Sc1nE";
+   //On vérifie que l'utilisateur est déjà connecté
+   if(!isset($_COOKIE["token"])){
+            // On le redirige vers la connexion Admin
+            header('Location:connexionAdmin.controller.php');
+    }
+    else{
+		//On décode le token
+		$decoded = JWT::decode($_COOKIE["token"], $key, array('HS256'));
+		$decoded_array = (array) $decoded;
+		//On vérifie que c'est un token valide
+		if (verificationToken($decoded_array)){
+			if($decoded_array['role']==="admin"){
+				if (isset($_POST["depart"])){ //On regarde si une information a été envoyée via le formulaire
+					$resultDepart=calculResultatDepart($_POST["depart"]); //On récupère le résultat des départements calculé par la fonction calculResultatDepart()
+				}
+				else{
+					$departs=getAllDepartement(); //On récupère tous les départements dans le tableau de tableau
+					$resultDepart= calculResultatDepart($departs[0]['id']); //On récupère le résultat des départements calculé par la fonction calculResultatDepart()
+        
+				}
+				include('../view/statistiques.php'); 
+			}
+			else{
+				// Si un Etudiant essaie d'accéder aux résultats on le redirige
+				header('Location:redirection.php');
+			}
+		}
+	}
+?>
