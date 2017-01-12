11fe3e2f1b4a769a51c55999f86a4721f5092a28
GodefroiRoussel <godefroi.roussel@etu.umontpellier.fr>
2017-01-12 19:35:18 +0100

    Update: gestion de pdo exception

--- model/connexionBD.php
+++ model/connexionBD.php
@@ -6,6 +6,7 @@ try
 		$pseudo='root';
 		$password='';
 		$pdo = new PDO('mysql:host=localhost;dbname=Piscine;charset=utf8',$pseudo,$password);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(Exception $e)
 	{
